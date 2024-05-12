3186cf6672235af8b3b28063c0d3ae47e51f21be
Ikechukwu Anyikwa <austineanyikwa@gmail.com>
2024-05-12 03:21:38 +0100

    added localhost to allowed hosts

--- pizza/settings.py
+++ pizza/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'aikay.pythonanywhere.com']
+
 
 
 # Application definition
